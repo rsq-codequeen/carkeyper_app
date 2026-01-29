eacb8d9b8c718ece6aa675e8eb8a0457b8948ed1
rsq-codequeen <159681694+rsq-codequeen@users.noreply.github.com>
2026-01-29 08:02:37 +0500

    added backend url to frontend

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,6 @@
+// Check if the app is running on a live website (Vercel) or locally
+const isProduction = window.location.hostname !== 'localhost';
 
-
-export const API_URL = 'http://localhost:8080/api';
+export const API_URL = isProduction 
+  ? 'https://carkeyper-backend.vercel.app/api' // Your new Vercel Backend
+  : 'http://localhost:8080/api';               // Your Local Backend
